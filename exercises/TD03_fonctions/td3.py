ed70f198abf981abaef2ea8878addb136cbf85d0
Jesinethan <jesinethan.parthipan@ens.uvsq.fr>
2022-10-04 11:10:45 +0200

    td3

--- exercises/TD03_fonctions/td3.py
+++ exercises/TD03_fonctions/td3.py
@@ -1,26 +1,17 @@
 #temps[0] : jours, temps[1]: heures, temps[2]: minutes, temps[3]: secondes
 
 def tempsEnSeconde(temps):
-    """
-    
-     Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde.
-    
-    
-    """
+    """Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
     s = 24*3600 * temps[0] + 3600 * temps[1] + 60 * temps[2] + temps[3]
     return s
 
 temps = (3,23,1,34)
-
 print(type(temps))
 print(tempsEnSeconde(temps))   
 
+
 def secondeEnTemps(seconde):
-    """
-    
-    Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument
-    
-    """
+    """Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument"""
     j = seconde // (24 * 3600)
     r = seconde % (24 * 3600)
     h = r // 3600
